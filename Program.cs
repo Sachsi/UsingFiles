5b14a2e6c9833f14d6375093920e89c3da7a8b7a
sachsetobias <t.sachse@esa-grimma.de>
2021-01-05 15:55:12 +0100

    created function to find selected files

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace UsingFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        
+        static IEnumerable<string> FindFiles(string folderName)
+        {
+            List<string> salesFiles = new List<string>();
+
+            var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+
+            foreach(var file in foundFiles)
+            {
+                //The file name will contain the ful path, so only check the end of it
+                if(file.EndsWith("sales.jason"))
+                {
+                    salesFiles.Add(file);
+                }
+            }
+            return salesFiles;
+        }
+    }
+
+}
